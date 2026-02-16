8cf7ca101589cbd2760cae19ae592ce1a2b818b8
Shane <robertsjshane@gmail.com>
2026-02-15 22:42:45 -0600

    Add GLTFLoader to load 3D model into scene

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js';
+const loader = new GLTFLoader();
+
+loader.load('./C7VETTEMODEL/c7unibody.glb', (gltf) => {
+  car = gltf.scene;
+  scene.add(car);
+});
