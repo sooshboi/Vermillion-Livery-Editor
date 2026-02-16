7c3b3c1fafc9f7332e284a9904d6114fdae0ac0d
Shane <robertsjshane@gmail.com>
2026-02-15 22:51:11 -0600

    Refactor GLTF model loading in script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-  const loader = new THREE.GLTFLoader();
-  loader.load(‘C7VETTEMODEL/c7unibody.gltf’, (gltf) => {
-    model = gltf.scene;
-    scene.add(model);
+<!-- index.html -->
+<script type="module">
+  import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.168.0/build/three.module.js';
+  import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/loaders/GLTFLoader.js';
+
+  // Now you can use it
+  const loader = new GLTFLoader();
+  loader.load('./C7VETTE_MODEL/c7unibody.glb', (gltf) => {
+    const car = gltf.scene;
+    scene.add(car);
   });
+</script>
