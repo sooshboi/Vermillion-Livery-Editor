51beec87899fe3084ae6853910b8790a25dba986
Shane <robertsjshane@gmail.com>
2026-02-15 22:45:03 -0600

    Fix GLTFLoader import and model loading syntax

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js';
-const loader = new GLTFLoader();
-
-loader.load('./C7VETTEMODEL/c7unibody.glb', (gltf) => {
-  car = gltf.scene;
-  scene.add(car);
-});
+  const loader = new THREE.GLTFLoader();
+  loader.load(‘C7VETTEMODEL/c7unibody.gltf’, (gltf) => {
+    model = gltf.scene;
+    scene.add(model);
+  });
