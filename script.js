e997e1b1a7653459a947b46496351c0bf1570e87
Shane <robertsjshane@gmail.com>
2026-02-15 22:30:18 -0600

    Initialize GLTFLoader and load car model

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-loader.load('./C7VETTEMODEL/c7unibody.glb', ...
+const loader = new GLTFLoader();
 
+loader.load('./C7VETTEMODEL/c7unibody.glb', (gltf) => {
+  car = gltf.scene;
+  scene.add(car);
+});
