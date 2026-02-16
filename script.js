d7ad01c824ae61660d729a17a3384e7d58e8cf03
Shane <robertsjshane@gmail.com>
2026-02-15 23:00:18 -0600

    Add imports for THREE.js and GLTFLoader

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
+
 // Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
