ad24bf249d5b49db5866f9b56164244ad4fcf294
Shane <robertsjshane@gmail.com>
2026-02-17 22:55:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 <script type="module">
-  import * as THREE from 'https://unpkg.com/three@0.172.0/build/three.module.js';
-
+ import * as THREE from 'https://unpkg.com/three@0.172.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.172.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.172.0/examples/jsm/loaders/GLTFLoader.js';
   const scene = new THREE.Scene();
   console.log('Three loaded', THREE);
-</script>
- 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(...);
 const renderer = new THREE.WebGLRenderer(...);
 scene.add(new THREE.AmbientLight(...));
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('three-canvas').appendChild(renderer.domElement);
-
 scene.add(new THREE.AmbientLight(0xffffff, 0.8));
    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
 dirLight.position.set(5, 5, 5);
 scene.add(dirLight);
 camera.position.z = 5;
-
    const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
-
    const loader = new GLTFLoader();
    let carModel = null;
 loader.load('./CAMAROMODELS/CAMAROUnibody.glb',
@@ -34,7 +30,6 @@ loader.load('./CAMAROMODELS/CAMAROUnibody.glb',
   undefined,
   (error) => console.error('Load error', error)
 );
-
 function animate() {
  requestAnimationFrame(animate);
  controls.update();
